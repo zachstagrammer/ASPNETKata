4cbdbe666983271e312c9b4730739782e563002e
Zach Phillips <zjohnny94@gmail.com>
2017-11-03 08:14:01 -0500

    Added Product list.

---
+++ ASPNETKata/Controllers/ProductController.cs
@@ -0,0 +1,100 @@
+﻿using ASPNETKata.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace ASPNETKata.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            var connectionString = "Server=localhost;Database=adventureworks;Uid=root;Pwd=unsecuresqlpass";
+            using (var conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                var list = conn.Query<Product>("SELECT * from Product");
+                return View(list);
+            }
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ASPNETKata/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPNETKata.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string ProductNumber { get; set; }
+        public double ListPrice { get; set; }
+        public bool MakeFlag { get; set; }
+        public bool FinishedGoodsFlag { get; set; }
+        public string Color { get; set; }
+        public int SafetyStockLevel { get; set; }
+        public int ReorderPoint { get; set; }
+        public int DaysToManufacture { get; set; }
+        public double StandardCost { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public DateTime SellStartDate { get; set; }
+    }
+}
