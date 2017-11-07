c104bb349ac526b06458913284f90692581fd85a
Zach Phillips <zjohnny94@gmail.com>
2017-11-07 14:26:43 -0600

    Removed some product attributes

--- ASPNETKata/Models/Product.cs
+++ ASPNETKata/Models/Product.cs
@@ -9,16 +9,5 @@ namespace ASPNETKata.Models
     {
         public int ProductId { get; set; }
         public string Name { get; set; }
-        public string ProductNumber { get; set; }
-        public double ListPrice { get; set; }
-        public bool MakeFlag { get; set; }
-        public bool FinishedGoodsFlag { get; set; }
-        public string Color { get; set; }
-        public int SafetyStockLevel { get; set; }
-        public int ReorderPoint { get; set; }
-        public int DaysToManufacture { get; set; }
-        public double StandardCost { get; set; }
-        public DateTime ModifiedDate { get; set; }
-        public DateTime SellStartDate { get; set; }
     }
 }
