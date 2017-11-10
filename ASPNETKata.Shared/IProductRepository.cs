04d01860505dc3b35f1235b2443ebbff5920d607
Zach Phillips <zjohnny94@gmail.com>
2017-11-10 13:37:19 -0600

    Added GetDetails to IProductRepository

--- ASPNETKata.Shared/IProductRepository.cs
+++ ASPNETKata.Shared/IProductRepository.cs
@@ -8,5 +8,6 @@ namespace ASPNETKata.Shared
         void DeleteProduct(int productId);
         void UpdateProduct(Product prod);
         void InsertProduct(Product prod);
+        Product GetDetails(int productId);
     }
 }
